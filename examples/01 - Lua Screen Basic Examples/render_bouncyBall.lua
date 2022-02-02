9b1d496e14bc4f8da1c107075ca92fa7e74726f8
NQ-Ligo <98467522+NQ-Ligo@users.noreply.github.com>
2022-02-02 15:13:50 +0100

    Update render_bouncyBall.lua
    Add track to have something for visual for players without increasing the complexity.

--- examples/01 - Lua Screen Basic Examples/render_bouncyBall.lua
+++ examples/01 - Lua Screen Basic Examples/render_bouncyBall.lua
@@ -2,29 +2,54 @@ local resX, resY = getResolution()
 local dt = 100 * getDeltaTime()
 
 -- initialize ball position once
-if not init then
-    init = true
-    ball = {}
-    ball.x = resX * math.random()
-    ball.y = resY * math.random()
-    ball.vx = 2 * math.random() - 1
-    ball.vy = 2 * math.random() - 1
-    ball.radius = math.min(resX, resY) / 8
+if not _init then
+    _init = true
+    
+    _time = getTime()
+    _lastPos = {}
+    
+    -- define the ball object
+    _ball = {
+        x = resX * math.random(),
+        y = resY * math.random(),
+        vel = 16,
+        dirX = math.random() - 0.5,
+        dirY = math.random() - 0.5,
+        radius = math.min(resX, resY) / 24
+    }
 end
 
--- animate ball position
-ball.x = ball.x + ball.vx * dt
-ball.y = ball.y + ball.vy * dt
-if ball.x < ball.radius or ball.x > (resX-ball.radius) then
-    ball.x = ball.x - ball.vx * dt
-    ball.vx = -ball.vx
+-- save the ball position
+if getTime() - _time > 0.1 then
+    table.insert(_lastPos, 1, {_ball.x, _ball.y})
+    if #_lastPos > 8 then table.remove(_lastPos,9) end
+    _time = getTime()
 end
-if ball.y < ball.radius or ball.y > (resY-ball.radius) then
-    ball.y = ball.y - ball.vy * dt
-    ball.vy = -ball.vy
+
+
+-- animate ball position
+_ball.x = _ball.x + _ball.dirX * _ball.vel * dt
+if _ball.x < _ball.radius or _ball.x > (resX-_ball.radius) then
+    _ball.x = _ball.x - _ball.dirX * _ball.vel * dt
+    _ball.dirX = -_ball.dirX
+end
+
+_ball.y = _ball.y + _ball.dirY * _ball.vel * dt
+if _ball.y < _ball.radius or _ball.y > (resY-_ball.radius) then
+    _ball.y = _ball.y - _ball.dirY * _ball.vel * dt
+    _ball.dirY = -_ball.dirY
 end
 
 -- render
 local layer = createLayer()
-addCircle(layer, ball.x, ball.y, ball.radius)
+
+-- draw ball positions snapshots
+for i,pos in ipairs(_lastPos) do
+    setNextFillColor(layer, 1, 1, 1, 0.21)
+    addCircle(layer, pos[1], pos[2],(1-i/#_lastPos) * _ball.radius)
+end
+
+-- draw the ball
+addCircle(layer, _ball.x, _ball.y, _ball.radius)
+
 requestAnimationFrame(1)
